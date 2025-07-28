b54eb29815a73366f0d16b5c105965903ce90f03
Auston <auston.ivison@spectrocloud.com>
2025-07-28 16:33:22 -0600

    Delete outputs.tf

--- outputs.tf
+++
@@ -1,8 +0,0 @@
-output "cluster_kubeconfig" {
-  value = spectrocloud_cluster_vsphere.cluster.kubeconfig
-}
-
-output "admin_kubeconfig" {
-  value = spectrocloud_cluster_vsphere.cluster.admin_kube_config
-}
-
